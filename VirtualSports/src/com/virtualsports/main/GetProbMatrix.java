426fac595c2bfbf89de8574afb56e452905de5bf
josephchen <josephchenhk@gmail.com>
2017-03-29 14:53:42 +0800

    finished sql connector class

--- VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
+++ VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
@@ -1,10 +1,20 @@
 package com.virtualsports.main;
 
+import java.sql.Connection;
+import com.virtualsports.data.SQLConnector;
+
 public class GetProbMatrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 		System.out.println("Get the probability matrix for a specified match.");
+		
+		SQLConnector sql = new SQLConnector();
+		
+		String query = ("select * from `order` " +
+		                "where updated_timestamp>'2017-03-22' " +
+		                "limit 4");
+		sql.select(query);
 	}
 
 }
