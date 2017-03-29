8dae8512a60382193eec2c8f15888ea2f8eb94b0
josephchen <josephchenhk@gmail.com>
2017-03-29 16:48:21 +0800

    finish getting parameters.

--- VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
+++ VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
@@ -1,20 +1,35 @@
 package com.virtualsports.main;
 
-import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import com.virtualsports.data.SQLConnector;
+import com.virtualsports.data.GetParams;
 
 public class GetProbMatrix {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		System.out.println("Get the probability matrix for a specified match.");
 		
-		SQLConnector sql = new SQLConnector();
+		int league_id = 36;
+		int homeTeam_id = 348;
+		int awayTeam_id = 24;
+		ArrayList<HashMap<String,Object>> homeTeamParams;
+		ArrayList<HashMap<String,Object>> awayTeamParams;
+		ArrayList<HashMap<String,Object>> leagueParams;
 		
-		String query = ("select * from `order` " +
-		                "where updated_timestamp>'2017-03-22' " +
-		                "limit 4");
-		sql.select(query);
+		GetParams getParams = new GetParams();
+		leagueParams = getParams.getLeagueParams(league_id);
+        homeTeamParams = getParams.getTeamParams(league_id, homeTeam_id);
+        awayTeamParams = getParams.getTeamParams(league_id, awayTeam_id);	
+        
+        System.out.println(leagueParams);
+        System.out.println(homeTeamParams);
+        System.out.println(awayTeamParams);
+        
+        getParams.close();
+		
 	}
+
 
 }
--- VirtualSports/src/com/virtualsports/pmatrix/ProbMatrix.java
+++ VirtualSports/src/com/virtualsports/pmatrix/ProbMatrix.java
@@ -2,5 +2,5 @@ package com.virtualsports.pmatrix;
 
 public class ProbMatrix {
 	
-
+	
 }
