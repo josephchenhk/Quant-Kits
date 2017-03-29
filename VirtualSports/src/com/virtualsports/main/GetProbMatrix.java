721b2dcafd3b137f8847490671fb31fe76f8cf7e
josephchen <josephchenhk@gmail.com>
2017-03-29 17:59:06 +0800

    update main

--- VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
+++ VirtualSports/src/com/virtualsports/main/GetProbMatrix.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import com.virtualsports.data.SQLConnector;
 import com.virtualsports.data.GetParams;
+import com.virtualsports.pmatrix.ProbMatrix;
 
 public class GetProbMatrix {
 	
@@ -14,6 +15,9 @@ public class GetProbMatrix {
 		int league_id = 36;
 		int homeTeam_id = 348;
 		int awayTeam_id = 24;
+		int hg = 2;
+		int ag = 1;
+		double t = 24.0;
 		ArrayList<HashMap<String,Object>> homeTeamParams;
 		ArrayList<HashMap<String,Object>> awayTeamParams;
 		ArrayList<HashMap<String,Object>> leagueParams;
@@ -28,7 +32,68 @@ public class GetProbMatrix {
         System.out.println(awayTeamParams);
         
         getParams.close();
+        
+        
+		double alphaH = Double.valueOf(homeTeamParams.get(0).get("alpha").toString());
+		double alphaA = Double.valueOf(awayTeamParams.get(0).get("alpha").toString());
+		double betaH = Double.valueOf(homeTeamParams.get(0).get("beta").toString());		
+		double betaA = Double.valueOf(awayTeamParams.get(0).get("beta").toString());
+		double gamma = Double.valueOf(leagueParams.get(0).get("gamma").toString());
 		
+		String lambdaStr = leagueParams.get(0).get("lambda").toString();
+		lambdaStr = lambdaStr.substring(1, lambdaStr.length()-1);
+		String[] lambdaStrList = lambdaStr.split(",");
+		double[] lambda = new double[lambdaStrList.length];
+		for (int i=0; i<lambdaStrList.length; i++){
+			lambda[i] = Double.parseDouble(lambdaStrList[i]);
+		}
+		
+		String muStr = leagueParams.get(0).get("mu").toString();
+		muStr = muStr.substring(1, muStr.length()-1);
+		String[] muStrList = muStr.split(",");
+		double[] mu = new double[muStrList.length];
+		for (int i=0; i<muStrList.length; i++){
+			mu[i] = Double.parseDouble(muStrList[i]);
+		}
+		
+		String rhoStr = leagueParams.get(0).get("rho").toString();
+		rhoStr = rhoStr.substring(1, rhoStr.length()-1);
+		String[] rhoStrList = rhoStr.split(",");
+		double[] rho = new double[rhoStrList.length];
+		for (int i=0; i<rhoStrList.length; i++){
+			rho[i] = Double.parseDouble(rhoStrList[i]);
+		}
+		
+		String xiStr = leagueParams.get(0).get("xi").toString();
+		xiStr = xiStr.substring(1, xiStr.length()-1);
+		String[] xiStrList = xiStr.split(",");
+		double[] xi = new double[xiStrList.length];
+		for (int i=0; i<xiStrList.length; i++){
+			xi[i] = Double.parseDouble(xiStrList[i]);
+		}
+		
+		/*
+		for (int i=0; i<6; i++){
+			System.out.print(lambda[i]+",");
+		}
+		System.out.println();
+		for (int i=0; i<6; i++){
+			System.out.print(mu[i]+",");
+		}
+		System.out.println();
+		for (int i=0; i<2; i++){
+			System.out.print(rho[i]+",");
+		}
+		System.out.println();
+		for (int i=0; i<2; i++){
+			System.out.print(xi[i]+",");
+		}
+		*/
+		
+		ProbMatrix pm = new ProbMatrix();
+		double[][] pMatrix;
+		pMatrix = pm.getProbMatrix(alphaH, betaH, alphaA, betaA, gamma, lambda, mu, rho, xi, hg, ag);
+		System.out.println(pMatrix[0][0]);
 	}
 
 
