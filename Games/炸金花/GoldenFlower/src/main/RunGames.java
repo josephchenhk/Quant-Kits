cd1e7adf2d8f9f536329b44236128565e3a27ca1
josephchen <josephchenhk@gmail.com>
2017-04-12 17:27:26 +0800

    run game

--- Games/炸金花/GoldenFlower/src/main/RunGames.java
+++ Games/炸金花/GoldenFlower/src/main/RunGames.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.ArrayList;
+import game.TenThousandGame;
+
 public class RunGames {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Õ¨½ð»¨£ºÈfÈËˆö");
+		TenThousandGame game1 = new TenThousandGame();
+		/*for (int i=0; i<52; i++){
+			System.out.println(game1.deck.get(i));
+		}*/		
+		System.out.println(game1.deck);
+		
+		/*
+		ArrayList<Integer> banker = game1.BankerDrawCards();
+		System.out.println(banker);
+		*/
 	}
 
 }
