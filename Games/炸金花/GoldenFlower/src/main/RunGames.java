46d608176f998caa2689dfa95e178465e641fca2
josephchen <josephchenhk@gmail.com>
2017-04-12 19:13:59 +0800

    delete print functions

--- Games/炸金花/GoldenFlower/src/main/RunGames.java
+++ Games/炸金花/GoldenFlower/src/main/RunGames.java
@@ -9,15 +9,7 @@ public class RunGames {
 		// TODO Auto-generated method stub
 		System.out.println("Õ¨½ğ»¨£ºÈfÈËˆö");
 		TenThousandGame game1 = new TenThousandGame();
-		/*for (int i=0; i<52; i++){
-			System.out.println(game1.deck.get(i));
-		}*/		
-		System.out.println(game1.deck);
-		
-		/*
-		ArrayList<Integer> banker = game1.BankerDrawCards();
-		System.out.println(banker);
-		*/
+	
 	}
 
 }
