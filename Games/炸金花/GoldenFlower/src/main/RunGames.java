a1e6279f72f1b82f10fa11cb52cc20a5696c9b38
josephchen <josephchenhk@gmail.com>
2017-04-13 15:27:47 +0800

    Finish TenThousandGame

--- Games/炸金花/GoldenFlower/src/main/RunGames.java
+++ Games/炸金花/GoldenFlower/src/main/RunGames.java
@@ -4,12 +4,30 @@ import java.util.ArrayList;
 import game.TenThousandGame;
 
 public class RunGames {
+	
+	private static int NUMSIM = 20000;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Õ¨½ð»¨£ºÈfÈËˆö");
-		TenThousandGame game1 = new TenThousandGame();
-	
+		int totalPay = 0;
+		int totalRec = 0; 
+		for (int i=0; i<NUMSIM; i++){
+			TenThousandGame game1 = new TenThousandGame();
+			//String[] betPlayer = {"player1","player2"};
+			//int[] betAmount = {1,1};
+			String[] betPlayer = {"player1"};
+			int[] betAmount = {1};
+			for (int j=0; j<betAmount.length; j++){
+				totalPay += betAmount[j];
+			}
+		    int pay = game1.Bet(betPlayer, betAmount);
+		    System.out.println("Pay:"+pay);
+		    totalRec += pay;
+		}
+		System.out.println("Total Pay: "+totalPay);
+		System.out.println("Total Receive: "+totalRec);
+		System.out.println("RTP: "+(totalRec*1.0/totalPay));
 	}
 
 }
