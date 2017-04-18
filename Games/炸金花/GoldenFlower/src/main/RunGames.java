a5fe0c221184715b9e0c143fed0a95829f2d41d5
josephchen <josephchenhk@gmail.com>
2017-04-18 11:15:00 +0800

    run dragon phoenix

--- Games/炸金花/GoldenFlower/src/main/RunGames.java
+++ Games/炸金花/GoldenFlower/src/main/RunGames.java
@@ -34,9 +34,34 @@ public class RunGames {
 		
 		
 		System.out.println("Õ¨½ð»¨£ºýˆøPŒ¦›Q");
+		double totalPay = 0;
+		double totalRec = 0; 
 		for (int i=0; i<NUMSIM; i++){
 			DragonPhoenix game2 = new DragonPhoenix();
+			
+			//bet player
+			String[] betPlayer = {}; //{"dragon"};
+			double[] betAmount = {}; //{1};			
+			for (int j=0; j<betAmount.length; j++){
+				totalPay += betAmount[j];
+			}		
+		    double pay = game2.WinBet(betPlayer, betAmount);
+		    System.out.println("Pay:"+pay);
+		    totalRec += pay;
+		    
+		    //bet type
+		    String[] betType = {"pair"};
+			double[] betTypeAmount = {1}; 
+		    for (int j=0; j<betTypeAmount.length; j++){
+				totalPay += betTypeAmount[j];
+			} 
+		    double pay2 = game2.TypeBet(betType, betTypeAmount);
+		    System.out.println("Pay2:"+pay2);
+		    totalRec += pay2;
 		}
+		System.out.println("Total Pay: "+totalPay);
+		System.out.println("Total Receive: "+totalRec);
+		System.out.println("RTP: "+(totalRec*1.0/totalPay));
 		
 	}
 
