873d9bd4efb56742e345ea3d067d0eda0c31b8d0
josephchen <josephchenhk@gmail.com>
2017-04-18 17:00:42 +0800

    finished

--- Games/炸金花/GoldenFlower/src/main/RunGames.java
+++ Games/炸金花/GoldenFlower/src/main/RunGames.java
@@ -6,7 +6,7 @@ import game.DragonPhoenix;
 
 public class RunGames {
 	
-	private static int NUMSIM = 20000;
+	private static int NUMSIM = 20000000;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -32,7 +32,7 @@ public class RunGames {
 		System.out.println("RTP: "+(totalRec*1.0/totalPay));
 		*/
 		
-		
+		long startTime = System.nanoTime();
 		System.out.println("Õ¨½ð»¨£ºýˆøPŒ¦›Q");
 		double totalPay = 0;
 		double totalRec = 0; 
@@ -40,28 +40,30 @@ public class RunGames {
 			DragonPhoenix game2 = new DragonPhoenix();
 			
 			//bet player
-			String[] betPlayer = {}; //{"dragon"};
-			double[] betAmount = {}; //{1};			
+			String[] betPlayer = {"phoenix"}; //{"dragon","phoenix"};
+			double[] betAmount = {1}; //{1};			
 			for (int j=0; j<betAmount.length; j++){
 				totalPay += betAmount[j];
 			}		
 		    double pay = game2.WinBet(betPlayer, betAmount);
-		    System.out.println("Pay:"+pay);
+		    //System.out.println("Pay *:"+pay);
 		    totalRec += pay;
 		    
 		    //bet type
-		    String[] betType = {"pair"};
-			double[] betTypeAmount = {1}; 
+		    String[] betType = {}; //{"pair8","flush","straight","straightFlush","leopard"};
+			double[] betTypeAmount = {}; 
 		    for (int j=0; j<betTypeAmount.length; j++){
 				totalPay += betTypeAmount[j];
 			} 
 		    double pay2 = game2.TypeBet(betType, betTypeAmount);
-		    System.out.println("Pay2:"+pay2);
+		    //System.out.println("Pay2 **:"+pay2);
 		    totalRec += pay2;
 		}
 		System.out.println("Total Pay: "+totalPay);
 		System.out.println("Total Receive: "+totalRec);
 		System.out.println("RTP: "+(totalRec*1.0/totalPay));
+		long estimatedTime = System.nanoTime() - startTime;
+		System.out.println("Elapsed Time: "+estimatedTime/(Math.pow(10, 9))+" seconds.");
 		
 	}
 
