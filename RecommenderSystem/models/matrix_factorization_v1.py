bce766b6fbf13498ccd8d5d1ae8c4dac62e075dd
josephchen <josephchenhk@gmail.com>
2017-04-24 14:59:50 +0800

    mf model for recommendation system

---
+++ RecommenderSystem/models/matrix_factorization_v1.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 14:31:44 2017
+
+@author: joseph.chen
+
+An implementation of matrix factorization
+
+Ref: http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/
+
+"""
+
+import numpy as np
+import random
+
+###############################################################################
+
+"""
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    P     : an initial matrix of dimension N x K
+    Q     : an initial matrix of dimension M x K
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices P and Q
+"""
+#def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+#    Q = Q.T
+#    for step in range(steps):
+#        for i in range(len(R)):
+#            for j in range(len(R[i])):
+#                if R[i][j] > 0:
+#                    eij = R[i][j] - np.dot(P[i,:],Q[:,j])
+#                    for k in range(K):
+#                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+#                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+#        eR = np.dot(P,Q)
+#        e = 0
+#        for i in range(len(R)):
+#            for j in range(len(R[i])):
+#                if R[i][j] > 0:
+#                    e = e + pow(R[i][j] - np.dot(P[i,:],Q[:,j]), 2)
+#                    for k in range(K):
+#                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+#        if e < 0.001:
+#            break
+#    return P, Q.T
+
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.02, beta=0.02):
+    Q = Q.T
+    for step in range(steps):
+        e = 0
+        N = 0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    N += 1.0
+                    eij = R[i][j] - np.dot(P[i,:],Q[:,j])
+                    e = e + pow(eij,2)
+                    for k in range(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+
+        e = np.sqrt(e/N)    
+                    
+        if e < 0.01:
+            break
+    
+    print("Number of iteration:",step+1)
+    print("Precision:",e)
+            
+    return P, Q.T
+#
+#def matrix_factorization2(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+#    QT = Q.T
+#    PT = P.T
+#
+#    for step in range(steps):
+#        e = 0
+#        N = 0
+#        for i in range(len(R)):
+#            for j in range(len(R[i])):
+#                if R[i][j] > 0:
+#                    N += 1.0
+#                    eij = R[i][j] - np.dot(P[i,:],QT[:,j])
+#                    e = e + pow(eij,2)
+#                    for k in range(K):
+#                        RQ = np.dot(R,Q)
+#                        PQTQ = np.dot(np.dot(P,QT),Q)
+#                        P[i][k] = P[i][k]*RQ[i][k]/PQTQ[i][k]
+#                        
+#                        PT = P.T
+#                        PTR = np.dot(PT,R)
+#                        PTPQT = np.dot(np.dot(PT,P),QT)
+#                        QT[k][j] = QT[k][j]*PTR[k][j]/PTPQT[k][j]
+#                        Q = QT.T
+#        
+#                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(QT[k][j],2) )
+#
+#
+#
+#        e = np.sqrt(e/N)    
+#        #print(e)            
+#        if e < 0.01:
+#            break
+#    
+#    print("Number of iteration:",step+1)
+#    print("Precision:",e)
+##            
+#    return P, Q
+
+def random_pick(some_list, probabilities):
+    x = random.uniform(0, 1)
+    cumulative_probability = 0.0
+    for item, item_probability in zip(some_list, probabilities):
+        cumulative_probability += item_probability
+        if x < cumulative_probability: break
+    return item
+
+def betting(R,k):
+    num_question = len(R[k])
+    bet = np.zeros(R.shape) 
+    for n in range(75):
+        num_question_candidate = random.choice(range(num_question)) + 1
+        questions = random.sample(range(num_question),num_question_candidate)
+        prob = []
+        for q in questions:
+            prob.append(R[k][q])
+        prob = [p/sum(prob) for p in prob]
+        question = random_pick(questions, prob)
+        bet[k][question] += 1    
+    return bet
+
+###############################################################################
+
+if __name__ == "__main__":
+    R = [
+         [5,3,0,1],
+         [4,0,0,1],
+         [1,1,0,5],
+         [1,0,0,4],
+         [0,1,5,4],
+        ]
+
+    R = np.array(R)
+
+    N = len(R)
+    M = len(R[0])
+    K = 2
+
+    P = np.random.rand(N,K)
+    Q = np.random.rand(M,K)
+
+    nP, nQ = matrix_factorization(R, P, Q, K)
+    nR = np.dot(nP, nQ.T)
+    print(nR)
+    
+    for n in range(3): # number of update
+        for m in range(len(nR)): # match (chatroom)
+            bet = betting(nR,m)
+            print("After Game {}".format(n+1))
+            print(bet)
+            print("\n")
+        
