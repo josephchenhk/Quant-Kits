2f7e260e0b02a358a44c4a080572abfafe216334
igoogol <josephchenhk@gmail.com>
2017-06-12 21:27:30 +0800

    test

---
+++ test/js/liaoxuefeng_test1.js
@@ -0,0 +1,18 @@
+﻿var Student = {
+    name: 'Robot',
+    height: 1.2,
+    run: function () {
+        console.log(this.name + ' is running...');
+    }
+};
+
+var xiaoming = {
+    name: '小明'
+};
+
+xiaoming.__proto__ = Student;
+
+xiaoming.name; // '小明'
+alert(xiaoming.run()); // 小明 is running...
+
+//alert('Hello, world');
