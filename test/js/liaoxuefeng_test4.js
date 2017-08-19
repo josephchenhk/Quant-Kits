b13f86194bde87b28dc3bdedad6113b700779186
igoogol <josephchenhk@gmail.com>
2017-08-19 08:36:43 +0800

    liaoxuefeng tutorial

---
+++ test/js/liaoxuefeng_test4.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+var obj = {
+    birth: 1990,
+    getAge: function (year) {
+        var b = this.birth; // 1990
+        //var fn = (y) => y - this.birth; // this.birth仍是1990
+		var fn;
+		fn = function(y){
+			return y - this.birth;
+		}
+        return fn.call({birth:2000}, year);
+    }
+};
+console.log(obj.getAge(2015)); // 25
